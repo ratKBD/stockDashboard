ba9168e51c9993d8418d6ab35b18fe9d41e9f38a
Vijayan Raghavan <vijayan@Priyeshs-Mac.local>
2024-07-24 17:07:31 +0530

    generateKeys1

--- server/config/server.js
+++ server/config/server.js
@@ -13,6 +13,12 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("APP_KEYS", ["dd24672f1c1d333d102064ccc352812a7a9b9e0814bb1e8561c226af2c6372f4", "8874fd4b3ed40083aedab55a953889b7f8d8d52a45e81ad6a1fa813396f20525"]);
+    keys: env.array("APP_KEYS", [
+      "dd24672f1c1d333d102064ccc352812a7a9b9e0814bb1e8561c226af2c6372f4",
+      "8874fd4b3ed40083aedab55a953889b7f8d8d52a45e81ad6a1fa813396f20525",
+    ]),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
